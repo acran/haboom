0e4bc4f6976cb199b8ae07e30b21b1f8afac84de
Roman Anasal <anasal@cip.ifi.lmu.de>
2020-09-01 19:15:47 +0200

    use Integers for numberInputs

--- frontend/Main.hs
+++ frontend/Main.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 import Data.Map (fromList)
-import Data.Text (Text)
+import Data.Text (Text, pack)
 import Reflex.Dom
 
 main :: IO ()
@@ -33,14 +33,18 @@ controlsDiv :: MonadWidget t m => m ()
 controlsDiv = divClass "controls row justify-content-center" $ do
   divClass "col-md-6 mt-2" $ do
     el "div" $ elClass "button" "btn btn-primary w-100" $ text "New game"
-    numberInput "width" "10"
-    numberInput "height" "10"
-    numberInput "mines" "20"
+    numberInput "width" 10
+    numberInput "height" 10
+    numberInput "mines" 20
 
-numberInput :: DomBuilder t m => Text -> Text -> m ()
+numberInput :: DomBuilder t m => Text -> Integer -> m ()
 numberInput label defValue = divClass "input-group mt-2" $ do
   divClass "input-group-prepend" $ divClass "input-group-text" $ text label
-  elAttr "input" ("value" =: defValue <> "type" =: "number" <> "class" =: "form-control") blank
+  elAttr "input" inputAttr blank
+  where
+    inputAttr = "value" =: (pack . show $ defValue)
+             <> "type" =: "number"
+             <> "class" =: "form-control"
 
 boardDiv :: MonadWidget t m => m ()
 boardDiv = divClass "card m-2" $ do
